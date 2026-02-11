223a47e140813e60306a6c19964f7710ff00cfe7
Akash Kumar <kumarakashjuly30@gmail.com>
2026-02-11 14:25:48 +0530

    sh

--- ec2-setup.sh
+++ ec2-setup.sh
@@ -8,8 +8,9 @@ sudo service docker start
 sudo usermod -a -G docker ec2-user
 
 echo "=== Installing Docker Compose ==="
-sudo curl -L "https://github.com/docker/compose/releases/latest/download/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+sudo curl -L "https://github.com/docker/compose/releases/download/v2.24.5/docker-compose-linux-x86_64" -o /usr/local/bin/docker-compose
 sudo chmod +x /usr/local/bin/docker-compose
+docker-compose --version
 
 echo "=== Installing Git ==="
 sudo yum install -y git
